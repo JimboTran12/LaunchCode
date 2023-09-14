897cf508929c0bc79bf0bd143e6f4e9b4b72a335
Jimmy <hungtran2003ti@gmail.com>
2023-09-13 23:17:42 -0500

    class 2 excercise

---
+++ 2class/boolncon_excercise.js
@@ -0,0 +1,85 @@
+let engineIndicatorLight= "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode  = 200;
+let shuttleSpeed = 15000;
+let fuelLevel = 2500;
+let engineTemp = 3500;
+//Question 2: the output will be: "engine are off"
+if (engineIndicatorLight === "green") {
+    console.log("engines have started");
+ } else if (engineIndicatorLight === "green blinking") {
+    console.log("engines are preparing to start");
+ } else {
+    console.log("engines are off");
+ }
+
+//Q3
+if (crewStatus === true) {
+    console.log("Crew Ready");
+}
+
+if (computerStatusCode === 200) {
+    console.log("Please stand by. Computer is rebooting");
+}
+else if (computerStatusCode === 400) {
+    console.log("Please stand by. Computer is rebooting");
+}
+else {
+    console.log("ALERT: Computer offline!")
+}
+
+if(shuttleSpeed > 17500) {
+    console.log("ALERT: Escape velocity reached!");
+}
+else if (shuttleSpeed < 8000) {
+    console.log("ALERT: Cannot maintain orbit!");
+}
+else {
+    console.log("Stable speed");
+}
+
+//Q4 Answer: Yes they will produce the same result
+if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
+
+ if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ }
+
+ //Q5
+ if(fuelLevel < 1000 || engineTemp > 3500 || engineIndicatorLight === "red blinking") {
+    console.log("ENGINE FAILURE IMMINENT!");
+ }
+ else if (fuelLevel <= 5000 || engineTemp > 2500) {
+    console.log("Check fuel level. Engines running hot.");
+ }
+ else if (fuelLevel > 20000 && engineTemperature <= 2500 ) {
+    console.log("Full tank. Engines good.");
+ }
+ else if (fuelLevel > 10000 && engineTemperature <= 2500) {
+    console.log("Fuel level above 50%. Engines good");
+ }
+ else if (fuelLevel > 5000 && engineTemperature <= 2500) {
+    console.log("Fuel level above 25%. Engines good.");
+ }
+ else {
+    console.log("Fuel and engine status pending...");
+ }
+
+ //Q6
+ let commandOverride = true;
+ if ((fuelLevel > 20000 && engineIndicatorLight !== "red blinking") || (commandOverride === true) ) {
+    console.log("Cleared to launch!");
+ }
+ else {
+    console.log("Launch scrubbed");
+ }
+
+
